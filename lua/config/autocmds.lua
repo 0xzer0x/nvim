f751cfe1d0208571b4e912aad8e90475baddec37
Youssef Fathy <32334265+0xzer0x@users.noreply.github.com>
2025-11-03 08:39:21 +0200

    feat(autocmds): autodetect roles tasks filetypes

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("CustomAutodetectAnsibleRoleTasksFiletype", { clear = true }),
+  desc = "Automatically set Ansible filetype for roles tasks files",
+  pattern = { "*/roles/*/tasks/*.{yml,yaml}", "*-playbook.{yml,yaml}" },
+  callback = function(ev)
+    vim.bo[ev.buf].filetype = "yaml.ansible"
+  end,
+})
